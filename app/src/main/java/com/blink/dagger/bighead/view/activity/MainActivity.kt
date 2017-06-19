0daf3ba6291c9d3db1752cd1aec0ab4677630193
luxiaogang <xiaogang.lu@oudot.cn>
2017-06-19 11:53:10 +0800

    add

--- app/src/main/java/com/blink/dagger/bighead/view/activity/MainActivity.kt
+++ app/src/main/java/com/blink/dagger/bighead/view/activity/MainActivity.kt
@@ -24,7 +24,6 @@ class MainActivity : AppCompatActivity(),View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         init()
     }
 
